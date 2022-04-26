9bf8fcd73da247e8a2691fbcdcb3c70b09e0681e
nyanyan <nhanhuynh_online@protonmail.com>
2022-04-26 18:55:58 +0200

    Fix javadoc

--- src/main/java/h04/function/FunctionOnRatioOfRuns.java
+++ src/main/java/h04/function/FunctionOnRatioOfRuns.java
@@ -6,7 +6,7 @@ import java.util.List;
 /**
  * Represents a function that accepts a double-valued argument and produces an int-valued result.
  *
- * <p>The function values are calculated by the ratio of runs and the number elements.
+ * <p>The function values are calculated by the ratio of runs and the number of elements.
  *
  * @author Nhan Huynh
  */
